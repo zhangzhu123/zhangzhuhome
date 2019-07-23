214fb9d9b85e4aab13eaae745af1d735b627979c
zhangzhu <zhangzhu_mail@163.com>
2019-07-24 00:11:40 +0800

    bk

--- web/PBR-Mobile/mainMobile.js
+++ web/PBR-Mobile/mainMobile.js
@@ -13,38 +13,38 @@ window.addEventListener('DOMContentLoaded', function(){
         camera.attachControl(canvas, true);   
         
         var pbr = new BABYLON.MDKPBRMaterial("pbr", scene);
-        pbr._reflectionTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("http://oydalubb3.bkt.clouddn.com/hdrvfx_pitpaSpecularHDR.dds", scene);
+        pbr._reflectionTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/hdrvfx_pitpaSpecularHDR.dds", scene);
         // pbr._reflectionTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("/9-mm/hdrvfx_pitpaSpecularHDR.dds", scene);
         pbr._reflectionTexture.gammaSpace = true;
         
         pbr._albedoColor = new BABYLON.Color3(1.0, 1.0, 1.0);
-        pbr._albedoTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_Albedo.tga.png", scene);
+        pbr._albedoTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_Albedo.tga.png", scene);
         // pbr._albedoTexture = new BABYLON.Texture("/9-mm/GunGS_Albedo.tga.png", scene);
         
         pbr._useMetal = true;
         pbr._reflectivityColor = new BABYLON.Color3(1.,1.,1.);
-        pbr._reflectivityTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_Metallic.tga.png", scene);
+        pbr._reflectivityTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_Metallic.tga.png", scene);
         // pbr._reflectivityTexture = new BABYLON.Texture("/9-mm/GunGS_Metallic.tga.png", scene);
         pbr._fresnelWeight = 0.;
         
         pbr._useGlossness = false;
         pbr._microSurface = 1.;
-        pbr._microSurfaceTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_Roughness.tga.png", scene);
+        pbr._microSurfaceTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_Roughness.tga.png", scene);
         // pbr._microSurfaceTexture = new BABYLON.Texture("/9-mm/GunGS_Roughness.tga.png", scene);
         
-        pbr._bumpTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_NormalGL.tga.png", scene);
+        pbr._bumpTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_NormalGL.tga.png", scene);
         // pbr._bumpTexture = new BABYLON.Texture("/9-mm/GunGS_NormalGL.tga.png", scene);
         // pbr._bumpTexture.level = 1.;
         pbr._invertNormalMapX = true;
         pbr._invertNormalMapY = true;
         
         // pbr._ambientColor = new BABYLON.Color3(3.0, 3.0, 3.0);
-        pbr._ambientTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_AO.tga.png", scene);
+        pbr._ambientTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_AO.tga.png", scene);
         // pbr._ambientTexture = new BABYLON.Texture("/9-mm/GunGS_AO.tga.png", scene);
         pbr._ambientTextureStrength = 1.4;
         
         pbr._emissiveColor = new BABYLON.Color3(2.0, 2.0, 2.0);
-        pbr._emissiveTexture = new BABYLON.Texture("http://oydalubb3.bkt.clouddn.com/GunGS_Emissive.tga.png", scene);
+        pbr._emissiveTexture = new BABYLON.Texture("https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/GunGS_Emissive.tga.png", scene);
         // pbr._emissiveTexture = new BABYLON.Texture("/9-mm/GunGS_Emissive.tga.png", scene);
 
         pbr._useTransparencyMask = false;
@@ -60,7 +60,7 @@ window.addEventListener('DOMContentLoaded', function(){
         
         //
         BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
-        BABYLON.SceneLoader.ImportMesh("", "", "http://oydalubb3.bkt.clouddn.com/9-mm.obj", scene, function (newMeshes) {
+        BABYLON.SceneLoader.ImportMesh("", "", "https://zhangzhu123.github.io/zhangzhuhome/asset/PBR-Mobile/9-mm.obj", scene, function (newMeshes) {
             newMeshes[0].material = pbr;
             model = newMeshes[0];
         });           
